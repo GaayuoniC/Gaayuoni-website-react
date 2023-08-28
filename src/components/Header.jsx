b7c1c54905fe3ae3fd605d0f9a2763b47a779653
Christian Gaayuoni <gayuoni@proton.me>
2023-08-29 00:05:02 +0200

    Add link to nav li

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,21 @@ import "./Header.css";
 export function Header() {
   return (
     <header>
-        <h1>Christian Gaayuoni</h1>
+      <h1>Christian Gaayuoni</h1>
       <nav>
         <ol className="nav-ol">
-          <li>Home</li>
-          <li>About me</li>
-          <li>CV</li>
-          <li>Contact</li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About me</a>
+          </li>
+          <li>
+            <a href="#">CV</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
         </ol>
       </nav>
     </header>
