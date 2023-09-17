8490542a2a3b8385a4c6a42b29b476082d26e682
Christian Gaayuoni <gayuoni@proton.me>
2023-09-17 08:12:44 +0200

    WIP

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,27 @@
 import "./Header.css";
 export function Header() {
   return (
-    <header>
-      <h1>Christian Gaayuoni</h1>
-      <nav>
-        <ol className="nav-ol">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About me</a>
-          </li>
-          <li>
-            <a href="#">CV</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ol>
-      </nav>
-      <img src={"Chris.jpeg"} alt="A picture of a man called Christian" />
-    </header>
+    <div className= "container-1">
+      <header>
+        <h1>Christian Gaayuoni</h1>
+        <nav className="nav-tabs" >
+          <ol >
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About me</a>
+            </li>
+            <li>
+              <a href="#">CV</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ol>
+        </nav>
+        <img src={"Chris.jpeg"} alt="A picture of a man called Christian" />
+      </header>
+    </div>
   );
 }
