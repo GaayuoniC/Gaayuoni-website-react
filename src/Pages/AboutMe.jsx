cf09486ee902676cbfd366c1fe03da436b11147c
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-16 10:12:17 +0100

    WIP: my life page work in progress

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,18 +1,56 @@
+import "./AboutMe.css";
+
 export function AboutMe() {
   return (
     <>
       <h1>Who am I?</h1>
       <div className="myLife-container">
-        <section>
+        <div className="myLife-item">
+          <h3>Early childhood</h3>
+          <section>
+            <p>
+              Born in Cape Coast, a city along the coast of Ghana in West
+              Africa. The first child of five siblings. Raised in Tamale in the
+              northern region of Ghana. Father was a teacher and mother was a
+              nurse. I started my basic education in Tamale, the Northern
+              regional capital in the north of Ghana.
+            </p>
+            <p></p>
+          </section>
+        </div>
+        <div className="myLife-item">
+          <h3>Teen years</h3>
+
           <p>
-            Born in Cape Coast, a city along the coast of Ghana in West Africa.
-            The first child of five siblings. Raised in Tamale in the northern
-            region of Ghana. Father was a teacher and mother was a nurse. I
-            started my basic education in Tamale, the Northern regional capital
-            in the north of Ghana.
+            I attended Tamale Secondary School from 1989 to 1994 and obtained my
+            General Certificate of Education, Ordinary Level (GCE O Level).
+            After graduation I gained admission to Prebyterian Boys Secondary
+            School in Accra, the capital city of Ghana. The school was
+            considered one of the best schools in Ghana. After two years of
+            studies in Geography,Economics and Government, I passed my Advanced
+            Level certification, the equivalent of level 100 in the United
+            Kingdom ie first year of university education.
           </p>
-          <p></p>
-        </section>
+        </div>
+        <div className="myLife-item">
+          <h3>Adulthood</h3>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+            consectetur ducimus vitae dolore odit tempore at nam id ex
+            accusamus, numquam itaque delectus doloribus quibusdam corporis
+            rerum velit autem suscipit dignissimos veritatis? Temporibus
+            doloremque ex placeat quidem eaque, voluptate numquam, pariatur
+            aperiam perferendis perspiciatis est recusandae adipisci dicta eius
+            debitis molestiae asperiores? Placeat aspernatur, repudiandae saepe
+            eaque nulla exercitationem numquam perspiciatis dolores unde
+            voluptates sapiente, reprehenderit qui atque eveniet quos provident
+            rerum tenetur temporibus sint. Quaerat ab, id ipsa quae labore
+            quisquam facilis amet beatae inventore sapiente perspiciatis ullam
+            cumque recusandae iste sed asperiores ad sint. Accusamus, itaque
+            labore! Accusantium.
+          </p>
+        </div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,20 @@ import "./index.css";
 import { RootLayout } from "../src/Pages/RootLayout";
 import { HomePage } from "../src/Pages/HomePage";
 import { AboutMe } from "./Pages/AboutMe";
-import {Cv} from "./Pages/Cv"
-import {Contact} from "./Pages/Contact"
+import { Cv } from "./Pages/Cv";
+import { Contact } from "./Pages/Contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ index: true, element: <HomePage /> },
-    {path: "aboutme", element: <AboutMe/>}, {path: "cv", element:<Cv/>},{
-      path: "contact", element: <Contact/>
-    }, {path: "homepage", element: <HomePage/>}
-  
-  
-  ],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "aboutme", element: <AboutMe /> },
+      { path: "cv", element: <Cv /> },
+      { path: "contact", element: <Contact /> },
+      { path: "homepage", element: <HomePage /> },
+    ],
   },
 ]);
 
