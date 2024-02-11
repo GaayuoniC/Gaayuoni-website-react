bab384d6ff981b220ef354d5e64d1ab5a84fd718
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-02-11 21:41:11 +0100

    initial commit

---
+++ src/components/RandomQuotes.jsx
@@ -0,0 +1,43 @@
+import "./RandomQuotes.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export function RandomQuotes() {
+  //Initialize the states
+  const [data, setData] = useState({});
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  //Using useEffect to fetch data from the api
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        const url = "https://type.fit/api/quotes"; //free quotes api
+        const { data } = await axios.get(url);
+        console.log(data); //debugging point for checking incoming data
+        setData(data);
+        //Random quote selection:
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomQuote = data[randomIndex].text;
+        const randomAuthor = data[randomIndex].name;
+        setQuote(randomQuote);
+        setAuthor(randomAuthor);
+      } catch (error) {
+        console.log(error);
+        alert("Oops data loading error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData(); //call the function to immediately fetch the data needed
+  }, []);
+
+  return (
+    <>
+      <p>randoms</p>
+    </>
+  );
+}
