7f4572b055852acdaaec08fd478f3cf33e2e4ca1
christian <gayuoni@proton.me>
2024-06-21 13:05:06 +0200

    mobile menu icon implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import "./NavBar.css";
 import { NavLink } from "react-router-dom";
 import { DigitalClock } from "./DigitalClock";
+import { useState } from "react";
+import { Image } from "@mui/icons-material";
+
+
 export function NavBar() {
+
+  const[open,setOpen]=useState(false)
+
+
   return (
     <header className="header-container">
       <div className="logo-area">
@@ -10,18 +18,47 @@ export function NavBar() {
       </div>
 
       <nav className="header-nav-container">
-        <NavLink to="/homepage">
+        <div className="desk-container"> 
+          <div className="desk-menu"> 
+          <NavLink to="/homepage">
           <li> Home</li>
-        </NavLink>
-        <NavLink to="/aboutme">
+          </NavLink>
+          <NavLink to="/aboutme">
           <li> My life</li>
-        </NavLink>
-        <NavLink to="/cv">
+          </NavLink>
+          <NavLink to="/cv">
           <li> CV</li>
-        </NavLink>
-        <NavLink to="/contact">
+          </NavLink>
+          <NavLink to="/contact">
           <li> Contact</li>
         </NavLink>
+        </div>
+        <div className="menu-img">   
+        <img id="mobile-img" src={open? "/close.png" :"/menu.png"} alt="menu image" width={30} height={30}  onClick={()=>setOpen((prev)=>!prev)}/>
+        </div>
+        </div>
+
+        {
+
+          open && <div className="mobile-menu">
+          <NavLink to="/homepage">
+           <li> Home</li>
+          </NavLink>
+          <NavLink to="/aboutme">
+           <li> My life</li>
+          </NavLink>
+          <NavLink to="/cv">
+           <li> CV</li>
+          </NavLink>
+          <NavLink to="/contact">
+           <li> Contact</li>
+          </NavLink>
+        
+ 
+         </div>
+        }
+
+        
       </nav>
     </header>
   );
